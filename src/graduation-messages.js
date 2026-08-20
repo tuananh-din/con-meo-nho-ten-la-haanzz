b0eb95d4a802aa0c23b0d804f6c1a6f6936ca5ea
tuananh-din <dta20030110@gmail.com>
2026-08-20 21:54:16 +0700

    Update _haanzz_ photos and messages

--- src/graduation-messages.js
+++ src/graduation-messages.js
@@ -1,16 +1,16 @@
 export const GRADUATION_MESSAGES = [
-  "Chúc mừng tốt nghiệp",
-  "Bạn đã làm được rồi",
-  "Tương lai thật sáng nhé",
-  "Tự hào về bạn lắm",
-  "Hành trình mới bắt đầu",
-  "Rực rỡ nhé tân khoa",
-  "Mọi nỗ lực đã nở hoa",
-  "Bước tiếp thật tự tin",
-  "Một cột mốc thật đẹp",
-  "Chúc bạn bay thật xa",
-  "Luôn tin vào chính mình",
-  "Thành công đang chờ bạn",
+  "Chậm một chút cũng được",
+  "Miễn là vẫn đang lớn lên",
+  "Hãy dịu dàng với chính mình",
+  "Một chiếc mèo thật đáng yêu",
+  "Giữ lại những ngày thật đẹp",
+  "Mọi điều rồi sẽ ổn thôi",
+  "Cứ vui theo cách của mình",
+  "Một góc nhỏ, thật nhiều thương",
+  "Meow một tiếng rồi mỉm cười",
+  "_haanzz_ đang tỏa sáng",
+  "Hôm nay cũng thật dễ thương",
+  "Bình yên trong thế giới nhỏ",
 ];
 
 export function randomMessage() {
